0ae8b98bc621a96eb6e666d5886d6111c1462e8b
desp0o <tornike.despotashvili@gmail.com>
2025-05-24 10:03:18 +0400

    create text extension

---
+++ unisplashClone/Helpers/Extensions/Text+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Text+Extension.swift
+//  unisplashClone
+//
+//  Created by Despo on 24.05.25.
+//
+
+import SwiftUI
+
+extension Text {
+  func customTextStyle(fontSize: CGFloat = 16, fontWeight: Font.Weight = .regular) -> some View {
+    self.font(.system(size: fontSize, weight: fontWeight, design: .rounded))
+      .foregroundStyle(Color.primary)
+  }
+}
